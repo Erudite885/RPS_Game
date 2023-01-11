4c0e47d369c2a824a63acf74080cea8f3be20ab5
Erudite885 <erudite885@gmail.com>
2023-01-11 16:03:27 +0100

    functions vs methos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("game begins");
+}
+
+
+
+startGameBtn.addEventListener("click", startGame);
