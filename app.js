be4cf8660bbe0343e17492af5dfbb09be26c5e12
Erudite885 <erudite885@gmail.com>
2023-01-11 16:29:22 +0100

    anonymous functions

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-const start = function startGame() {
-  console.log("game begins");
-};
+// const start = function() {
+//   console.log("the game begins");
+// };
 
-startGameBtn.addEventListener("click", start);
+startGameBtn.addEventListener("click", function () {
+  console.log("the game begins in anonymous function");
+});
