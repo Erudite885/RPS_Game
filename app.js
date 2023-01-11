a5b811521415d1ff2675a24a7df8dc94f928075c
Erudite885 <erudite885@gmail.com>
2023-01-11 16:47:18 +0100

    adding user choices

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-// const start = function() {
-//   console.log("the game begins");
-// };
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid Choice! ${DEFAULT_USER_CHOICE} selected`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
 
 startGameBtn.addEventListener("click", function () {
+    if (gameIsRunning){
+        return
+    }
   console.log("the game begins in anonymous function");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
